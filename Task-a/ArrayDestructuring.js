a50ba8d1ef224d2f7f36b7f3427c99f4a874110b
amnaghori <87307038+amnaghori@users.noreply.github.com>
2021-08-31 02:14:02 +0500

    Create ArrayDestructuring.js

---
+++ Task-a/ArrayDestructuring.js
@@ -0,0 +1,9 @@
+//Array Destructuring
+var arr = [10, 20, 30];
+function show(z){
+return z;
+}
+var [value1, value2, value3] = show(arr);
+console.log(value1);
+console.log(value2);
+console.log(value3);
